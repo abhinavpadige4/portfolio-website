d501a5e1a0f38bca3584e016c5b2a01214131430
abhinav <abhinavpadige06@gmail.com>
2026-09-23 03:16:45 +0530

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ module.exports = {
   swcMinify: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**', pathname: '**' }
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' }
     ]
-  },
-  eslint: { ignoreDuringBuilds: true }
-}
+  }
+};
