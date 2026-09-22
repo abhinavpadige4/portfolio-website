35517b53c935891a87cb284a8080c13804af6b51
abhinav <abhinavpadige06@gmail.com>
2026-09-23 03:31:09 +0530

    Fix src/components/Navbar.tsx (E2B r2)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar: React.FC = () => {
     <nav className="bg-gray-800 text-white px-6 py-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <span className="text-xl font-bold">MyApp</span>
-        <div className="flex gap-4">
-          <a href="#" className="hover:text-gray-300">Home</a>
-          <a href="#" className="hover:text-gray-300">About</a>
-          <a href="#" className="hover:text-gray-300">Contact</a>
-        </div>
+        <ul className="flex space-x-6">
+          <li><a href="#" className="hover:text-gray-300">Home</a></li>
+          <li><a href="#" className="hover:text-gray-300">About</a></li>
+          <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+        </ul>
       </div>
     </nav>
   );
